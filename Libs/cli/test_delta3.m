2d96ca0db8cad49c4f73b60d14e0c4b3d8adb457
Alastair Pharo <asppsa@gmail.com>
2011-05-09 14:48:22 +1000

    Remove 'cli' folder, which is no longer in use

--- Libs/cli/test_delta3.m
+++
@@ -1,9 +0,0 @@
-function delta = test_delta3 (x, u)
-    y = x(1); pi = x(2); i = x(3); q = x(4);
-    
-    delta = [...
-        -0.2*y-0.5*(i-pi)+0.2*q;
-        0.4*y;
-        u;
-        i-pi];
-end
