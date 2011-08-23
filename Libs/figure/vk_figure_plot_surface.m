01e5f28995db830292a141710d9219ec22ed4ce0
Alastair Pharo <asppsa@gmail.com>
2011-08-23 20:48:25 +1000

    Replaced vk_figure_plot_surface() with a shell function, and a set of sub functions.

--- Libs/figure/vk_figure_plot_surface.m
+++ Libs/figure/vk_figure_plot_surface.m
@@ -28,111 +28,28 @@ function vk_figure_plot_surface(V, colour, method, alpha_val)
     hold on;
     grid on;
 
-    err = 0;
-    if (strcmp(method, 'qhull'))
-        try
-            % T is a set of triangles that together trace a surface around
-            % V.
-            T = convhulln(V);
-
-            % Attempt to add lighting effects too.
-            try
-                camlight left;
-                camlight right;
-                lighting gouraud;
-
-                %   p = trisurf(T, V(:,1), V(:,2), V(:,3));
-                %   set(p,'FaceColor','yellow','EdgeColor','none');
-                for i = 1:length(T)
-                    p = patch(V(T(i,:), 1), V(T(i,:), 2), V(T(i,:), 3), ...
-                        colour);
-                    set(p,'FaceColor',colour,'EdgeColor','none');
-                    alpha(p, alpha_val);
-                end
-            catch
-                warning('Couldn''t set lighting');
-
-                for i = 1:length(T)
-                    p = patch(V(T(i,:), 1), V(T(i,:), 2), V(T(i,:), 3), ...
-                        [0,1,1]);
-                    set(p,'FaceColor',colour,'EdgeColor','black');
-                end
-            end
-        catch
-            % If we couldn't do a convexhull, we probably don't have enough
-            % points. So, do a scatterplot instead.
-            exception = lasterror();
-            err = 1;
-            warning('Couldn''t do a convex hull around points, so reverting to a scatterplot');
-        end
-    elseif (strcmp(method, 'isosurface') || strcmp(method, 'isosurface-smooth'))
-        try
-            xax = sort(unique(V(:,1)));
-            yax = sort(unique(V(:,2)));
-            zax = sort(unique(V(:,3)));
-
-            volume = zeros(length(yax), length(xax), length(zax));
-
-            Vsorted = sortrows(V);
-            Vidx = 1;
-            for x = 1:length(xax)
-                for y = 1:length(yax)
-                    for z = 1:length(zax)
-                        if (all(Vsorted(Vidx, :) == [xax(x),yax(y),zax(z)]))
-                            % If this is the edge of the graph, make is
-                            volume(y, x, z) = 2;
-                            Vidx = Vidx + 1;
-                        end
-                        if (Vidx > size(V, 1))
-                            break;
-                        end
-                    end
-                    if (Vidx > size(V, 1))
-                        break;
-                    end
-                end
-                if (Vidx > size(V, 1))
-                    break;
-                end
-            end
-
-            if (strcmp(method, 'isosurface-smooth'))
-                volume = smooth3(volume);
-            end
-
-            %% Octave performs isosurface differently from MATLAB
-            if (exist('octave_config_info'))
-                [xx, yy, zz] = meshgrid(xax, yax, zax);
-                h_iso = patch(isosurface(xx, yy, zz, volume, 1));
-                isonormals(xx,yy,zz,volume,h_iso);
-            else
-                h_iso = patch(isosurface(xax, yax, zax, volume, 1));
-                isonormals(xax,yax,zax,volume,h_iso);
-                h_caps = patch(isocaps(xax,yax,zax,volume,1));
-                set(h_caps,'FaceColor',colour, 'EdgeColor', 'none');
-                alpha(h_caps, alpha_val);
-                alpha(h_iso, alpha_val);
-            end
-
-            set(h_iso,'FaceColor',colour);
-
-            try
-                camlight left;
-                camlight right;
-                lighting gouraud;
-                set(h_iso,'EdgeColor','none');
-            catch
-                warning('Couldn''t set lighting');
-            end
-        catch
-            exception = lasterror();
-            err = 1;
-            warning('Couldn''t do a isosurface around points, so reverting to a scatterplot');
-        end
+    if (length(method) > 6 && strcmp(method(end-6:end), '-smooth'))
+        method = method(1:end-7);
+        smoothopt = {'smooth', 1};
+    else
+        smoothopt = {};
     end
 
-    if (err || strcmp(method, 'scatter'))
-        scatter3(V(:, 1), V(:, 2), V(:, 3), 10, colour);
+    fallbackfn = @vk_figure_plot_surface_scatter;
+    if (exist(['vk_figure_plot_surface_',method]))
+        plotfn = eval(['@vk_figure_plot_surface_',method]);
+    else
+        plotfn = fallbackfn;
+    end
+
+    err = 0;
+    try
+        plotfn(V, colour, alpha_val, smoothopt{:});
+    catch
+        err = 1;
+        exception = lasterror();
+        warning(['Couldn''t plot kernel using "', method, '" method. Reverting to a scatterplot']);
+        fallbackfn(V, colour, alpha_val);
     end
 
     if (err)
