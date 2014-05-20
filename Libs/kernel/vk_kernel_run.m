b660dbd980a4661703cd9556ab86d63f41d58ebb
Alastair Pharo <asppsa@gmail.com>
2014-05-20 19:46:36 +1000

    Make vk_kernel_run able to use either algorithm

--- Libs/kernel/vk_kernel_run.m
+++ Libs/kernel/vk_kernel_run.m
@@ -1,17 +1,19 @@
 %% VK_KERNEL_RUN Run a kernel calculation from a file or structure.
-%   This function takes as input either a filename containing a project, or a
-%   structure representing a project, and runs the viability kernel calculation
-%   contained within.  Then, it either returns the result, or saves it into a
+%   This function takes as input a string representing the name of the
+%   algorithm to run (either 'inclusion' or 'exclusion') and either a
+%   filename containing a project, or a structure representing a
+%   project, and runs the viability kernel calculation contained
+%   within.  Then, it either returns the result, or saves it into a
 %   file.
 %
-%   vk_kernel_run(FILENAME)  Runs the project contained in FILENAME, and when
-%   complete saves the result back into that file.
+%   vk_kernel_run(ALG, FILENAME) Runs ALG on the project contained in
+%   FILENAME, and when complete saves the result back into that file.
 %
-%   vk_kernel_run(FILE1, FILE2) Runs the project contained in FILE1, and when
-%   complete, saves the result into FILE2.
+%   vk_kernel_run(ALG, FILE1, FILE2) Runs the project contained in
+%   FILE1, and when complete, saves the result into FILE2.
 %
-%   proj2 = vk_kernel_run(PROJ1) Runs the project represented by PROJ1 and
-%   returns a new structure.
+%   proj2 = vk_kernel_run(ALG, PROJ1) Runs the project represented by
+%   PROJ1 and returns a new structure.
 %
 % Examples
 %   % Load a file into a structure
@@ -21,8 +23,8 @@
 %   proj.steps = 2;
 %   proj.use_controldefault = 1;
 %   proj.controldefault = 0;
-%   % Re-run the kernel.
-%   proj = vk_kernel_run(proj);
+%   % Re-run the inclusion algorithm.
+%   proj = vk_kernel_run('inclusion', proj);
 %   % Save the result.
 %   vk_project_save(project, 'Projects/newproject.mat');
 %
@@ -31,7 +33,7 @@
 % See also: vikaasa_cli
 
 %%
-%  Copyright 2011 Jacek B. Krawczyk and Alastair Pharo
+%  Copyright 2014 Jacek B. Krawczyk and Alastair Pharo
 %
 %  Licensed under the Apache License, Version 2.0 (the "License");
 %  you may not use this file except in compliance with the License.
@@ -44,9 +46,13 @@
 %  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %  See the License for the specific language governing permissions and
 %  limitations under the License.
-function varargout = vk_kernel_run(varargin)
-    if (nargin == 0)
-        error('You must specify at least one input.');
+function varargout = vk_kernel_run(alg, varargin)
+    if (nargin < 2)
+        error('You must specify at least two inputs.');
+    end
+
+    if ~strcmp(alg,'inclusion') && ~strcmp(alg, 'exclusion')
+        error('The algorithm must be either "inclusion" or "exclusion".');
     end
 
     if (ischar(varargin{1}))
@@ -69,6 +75,14 @@ function varargout = vk_kernel_run(varargin)
             'progress_fn', @(x) fprintf('\r%6.2f%% done', (x/computations)*100));
     end
 
+
+    %% determine the control algorithm
+    if alg == 'inclusion'
+      compute_fn = @vk_kernel_compute;
+    else % exclusion
+      compute_fn = @vk_iss_kernel_compute;
+    end
+
     %% Display debugging information
     if (project.debug)
         % Output the settings to the screen for debugging.
@@ -83,8 +97,8 @@ function varargout = vk_kernel_run(varargin)
     tic;
     fprintf('RUNNING ALGORITHM\n');
     success = 0; err = 0;
-    %try
-        V = vk_kernel_compute(K, f, c, options);
+    try
+        [V, NV, viable_paths, nonviable_paths] = compute_fn(K, f, c, options);
 
         if (options.cancel_test_fn())
             fprintf('CANCELLED\n');
@@ -92,29 +106,32 @@ function varargout = vk_kernel_run(varargin)
             fprintf('FINISHED\n');
             success = 1;
         end
-    %catch
-    %    exception = lasterror();
-    %    error(exception);
-    %    fprintf('ERROR: %s\n', exception.message);
-    %    err = 1;
-    %end
+    catch
+        exception = lasterror();
+        error(exception);
+        fprintf('ERROR: %s\n', exception.message);
+        err = 1;
+    end
 
     comp_time = toc;
 
     % Save the results into our state structure if successful.
     if (success)
         project.V = V;
+        project.viable_paths = viable_paths;
+        project.NV = NV;
+        project.nonviable_paths = nonviable_paths;
         project.comp_time = comp_time;
         project.comp_datetime = ...
             sprintf('%i-%i-%i %i:%i:%i', cl(1), cl(2), cl(3), cl(4), cl(5), cl(6));
 
-        if (nargin > 1)
+        if (nargin > 2)
             % If a separate file was specified, save to that.
-            save(varargin{2}, '-struct', 'project');
+            vk_project_save(project, varargin{2});
         elseif (nargout == 0 && ischar(varargin{1}))
             % Otherwise, if no other option was given, save back to the
             % original file.
-            save(varargin{1}, '-struct', 'project');
+            vk_project_save(project, varargin{1});
         end
 
         % If a return is expected, give back the structure.
